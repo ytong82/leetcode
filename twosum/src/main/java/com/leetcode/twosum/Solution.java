efcb24f605e26b9b4af39f8aba05c26ee47122dd
David <ytong82@aliyun.com>
2018-09-27 23:43:31 +0800

    Two Sum - Hash Table Solution

--- twosum/src/main/java/com/leetcode/twosum/Solution.java
+++ twosum/src/main/java/com/leetcode/twosum/Solution.java
@@ -1,17 +1,21 @@
 package com.leetcode.twosum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
     	int[] result = new int[2];  	
-    	for (int i=0; i<nums.length; i++) {
-    		for (int j=i+1; j<nums.length; j++) {
-    			if (nums[j] == target - nums[i]) {
-    				result[0] = i;
-    				result[1] = j;
-    				System.out.println(i + " " + j);
-    				break;
-    			}
-    		}
+    	
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	for (int index=0; index<nums.length; index++) {
+			int key = target - nums[index];
+			if (map.containsKey(key)) {
+				result[0] = map.get(key);
+				result[1] = index;
+				break;
+			}
+    		map.put(nums[index], index);
     	}
     	return result;
     }
