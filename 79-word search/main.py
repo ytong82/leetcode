9a8d9ae8764e8ff3035a6bbfbc516021bed4d174
David Tong <ytong82@towing.top>
2020-03-02 23:12:46 -0800

    3/2/2020

--- 79-word search/main.py
+++ 79-word search/main.py
@@ -1,5 +1,3 @@
-import copy
-
 class Solution:
     def exist(self, board, str):
         self.height = len(board)
@@ -7,39 +5,31 @@ class Solution:
             return False
         self.width = len(board[0])
 
-        book = [[0 for i in range(self.width)] for j in range(self.height)]
-
         if_exist = False
         for i in range(self.height):
             for j in range(self.width):
-                if_exist = self.__exist(board, str, i, j, copy.deepcopy(book))
-                if if_exist:
-                    return if_exist
+                if self.__exist(board, str, i, j):
+                   return True
 
-        return if_exist
+        return False
 
-    def __exist(self, board, str, i, j, book):
-        book[i][j] = 1
+    def __exist(self, board, str, i, j):
+        if i < 0 or i >= self.height or j < 0 or j >= self.width:
+            return False
+
         if board[i][j] == str[0]:
             if len(str) == 1:
                 return True
+            board[i][j] = 0
+            res = self.__exist(board, str[1:], i-1, j) \
+                  or self.__exist(board, str[1:], i+1, j) \
+                  or self.__exist(board, str[1:], i, j-1) \
+                  or self.__exist(board, str[1:], i, j+1)
+            if res:
+                return True
             else:
-                if i > 0:
-                    if book[i-1][j] == 0:
-                        if self.__exist(board, str[1:], i-1, j, copy.deepcopy(book)):
-                            return True
-                if i < self.height - 1:
-                    if book[i+1][j] == 0:
-                        if self.__exist(board, str[1:], i+1, j, copy.deepcopy(book)):
-                            return True
-                if j > 0:
-                    if book[i][j-1] == 0:
-                        if self.__exist(board, str[1:], i, j-1, copy.deepcopy(book)):
-                            return True
-                if j < self.width - 1:
-                    if book[i][j+1] == 0:
-                        if self.__exist(board, str[1:], i, j+1, copy.deepcopy(book)):
-                            return True
+                board[i][j] = str[0]
+                return False
         else:
             return False
 
@@ -49,9 +39,16 @@ board = [
     ['S', 'F', 'C', 'S'],
     ['A', 'D', 'E', 'E'],
 ]
+
 word = "ABCCED"
 word = "SEE"
 word = "ABCB"
+
+board = [["a", "a"]]
+word = "aaa"
+
+board = [["C","A","A"],["A","A","A"],["B","C","D"]]
+word = "AAB"
 
 sol = Solution()
 print(sol.exist(board, word))
